13edf9416f03ead97ef4e52acf8b2ce654ba5c94
Gordeev311 <152351140+Gordeev311@users.noreply.github.com>
2024-02-29 13:23:40 +1000

    Add files via upload

---
+++ Gordeev_Muzey/ApplicationData/AppcConnect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gordeev_Muzey.Baza;
+
+namespace Gordeev_Muzey.ApplicationData
+{
+    internal class AppConnect
+    {
+        public static MuzeyEntities model0db;
+    }
+}
---
+++ Gordeev_Muzey/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Gordeev_Muzey.ApplicationData;
+using Gordeev_Muzey.Baza;
+using Gordeev_Muzey.Pages;
+
+namespace Gordeev_Muzey
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AppConnect.model0db = new MuzeyEntities();
+            DtGridMyzey.ItemsSource = MuzeyEntities.GetContext().Sklad.ToList();
+            AppFrame.MainFrame = FrmMain;
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
+
+        private void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.MainFrame.Navigate(new PageAdd());
+            
+
+
+        }
+
+
+    }
+    }
